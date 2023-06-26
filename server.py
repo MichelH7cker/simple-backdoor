f0a77810d18dd9df6acb90c0b119665dab6a32da
MichelH4cker <michel.hecker@usp.br>
2023-06-26 15:05:32 -0300

    feat: create a socket and add bind, listen and connect functions

--- server.py
+++ server.py
@@ -3,7 +3,26 @@ import sys
 
 # CREATE A SERVER, RECEIVE FILE FROM CLIENT AND WRITE IT IN A FILE
 def server_init(server_address, port_address):
-    print("server_init")
+    # CREATE SOCKET
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # ASSOCIATE SOCKET WITH AN ADRRESS AND PORT
+    server.bind((server_address, port_address))
+
+    # PUT SOCKET TO LISTEN
+    server.listen()
+
+    print(f"[+] Server started at {server_address}:{port_address}")
+
+    while True:
+        # WAIT TO CLIENT CONNECT
+        print("...WAITING...")
+        client, client_address = server.accept()
+        print(f"[+] Client {client_address} connected")
+
+        # CLOSE CONNECTION WITH CLIENT
+        client.close()
+        print(f"\n[+] Client {client_address} disconnected")
 
 # EXECUTE PROGRAM
 if __name__ == '__main__':
